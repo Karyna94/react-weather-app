101fb50b2e3a901f3d08c60c066d6176284b2d10
Karyna <nosova2683@gmail.com>
2022-12-25 21:33:32 +0200

    Import bootstrat

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Karyna Trotsenko <a href="https://github.com/Karyna94/react-weather-app" target="_blank" rel="noreferrer" >open-sourced on GitHub</a> 
       </footer>
+      </div>
     </div>
   );
 };
