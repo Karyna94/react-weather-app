a6faa6d0d48c410a93401ae191b4441b7958f38b
Karyna <nosova2683@gmail.com>
2022-12-26 07:38:31 +0200

    Created stcructure of HTML page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import Weather from "./Weather"
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+        <Weather />
       <footer>
         This project was coded by Karyna Trotsenko <a href="https://github.com/Karyna94/react-weather-app" target="_blank" rel="noreferrer" >open-sourced on GitHub</a> 
       </footer>
